63b3cb80d650906806e4f3f51fcf152e9a0dc421
Tang Vinh Tai (FHM.FCT) <TaiTV3@fsoft.com.vn>
2019-07-09 15:24:01 +0700

    Updated Origin Master

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+@NgModule({
+  declarations: [MainComponent, HomeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
